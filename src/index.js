218b78fc0d9520671830def52be69735b7aa6849
sam <abluecar@gmail.com>
2024-07-24 14:04:35 -0400

    feat: Finish dropdown menu (html, css, js)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 // index.js
 import './style.css';
+import 'boxicons';
+
+const menuButton = document.getElementById('menu-button');
+const dropdown = document.getElementById('dropdown');
+const arrow = document.getElementById('arrow');
+
+const toggleDropdown = function () {
+  dropdown.classList.toggle('show');
+  arrow.classList.toggle('arrow-up');
+};
+
+menuButton.addEventListener('click', (event) => {
+  event.stopPropagation();
+  toggleDropdown();
+});
+
+// If user clicks on somewhere other than the menu, close menu
+document.addEventListener('click', () => {
+  if (dropdown.classList.contains('show')) toggleDropdown();
+});
